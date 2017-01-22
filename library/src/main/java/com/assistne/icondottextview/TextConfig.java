a9d2dea1888007a153898d8173140d63085e2cd4
AssIstne <liangjiaquan@wbiao.cn>
2017-01-22 18:06:55 +0800

    move layout into TextConfig

--- library/src/main/java/com/assistne/icondottextview/TextConfig.java
+++ library/src/main/java/com/assistne/icondottextview/TextConfig.java
@@ -2,7 +2,12 @@ package com.assistne.icondottextview;
 
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.ColorInt;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.TextUtils;
 
 /**
  * Created by assistne on 17/1/20.
@@ -11,15 +16,20 @@ import android.support.annotation.ColorInt;
 // TODO: 17/1/20 限制文本长度, 过长省略
 // TODO: 17/1/20 文本改变的情况
 
-public class TextConfig {
+public class TextConfig implements Config {
 
-    private static final int DEFAULT_SIZE = 10;
+    private static final int DEFAULT_SIZE = 36;
     @ColorInt
     private static final int DEFAULT_COLOR = Color.RED;
+    private static final int DEAFULT_MAX_WIDTH = 400;
 
     int size = DEFAULT_SIZE;
+    int maxWidth = DEAFULT_MAX_WIDTH;
     @ColorInt int color = DEFAULT_COLOR;
     String text;
+
+    private TextPaint mTextPaint;
+    private Layout mLayout;
 
     public TextConfig(TypedArray typedArray) {
         if (typedArray != null) {
@@ -27,15 +37,37 @@ public class TextConfig {
             color = typedArray.getColor(R.styleable.IconDotTextView_dot_color, DEFAULT_COLOR);
             text = typedArray.getString(R.styleable.IconDotTextView_dot_text);
         }
+        init();
     }
 
     public TextConfig(String text, int size, @ColorInt int color) {
         this.text = text;
         this.size = size;
         this.color = color;
+        init();
     }
 
+    private void init() {
+        initPaint();
+        initLayout();
+    }
+
+    private void initPaint() {
+        mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(color);
+        mTextPaint.setTextSize(size);
+    }
+
+    private void initLayout() {
+        if (!TextUtils.isEmpty(text)) {
+            mLayout = new StaticLayout(text, mTextPaint, 200, Layout.Alignment.ALIGN_CENTER, 1f, 0f, true);
+        }
+    }
+
+
     public int getWidth() {
+//        int desiredWidth = (int) Math.ceil(Layout.getDesiredWidth(text, mTextPaint));
+//        return Math.min(desiredWidth, maxWidth);
         return 100;
     }
 
