0591072cae59a76c4b2d89bdc5e32192397a042b
AssIstne <liangjiaquan@wbiao.cn>
2017-01-23 16:16:15 +0800

    adjust the demo

--- example/app/src/main/java/com/assistne/example/MainActivity.java
+++ example/app/src/main/java/com/assistne/example/MainActivity.java
@@ -1,7 +1,10 @@
 package com.assistne.example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Row");
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return new RowFragment();
+                    case 1:
+                        return new ColumnFragment();
+                    case 2:
+                        return new RowReverseFragment();
+                    default:
+                        return new ColumnReverseFragment();
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        setTitle("Row");
+                        break;
+                    case 1:
+                        setTitle("Column");
+                        break;
+                    case 2:
+                        setTitle("Row Reverse");
+                        break;
+                    default:
+                        setTitle("Column Reverse");
+                }
+            }
+        });
     }
 }
