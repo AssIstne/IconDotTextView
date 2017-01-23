b583e7339b44227b1ea3b1eba18f25167a2103c7
AssIstne <liangjiaquan@wbiao.cn>
2017-01-23 16:04:26 +0800

    Revert "add some example into demo"
    This reverts commit 72b8e5c55bb3cb8022f8014557d4fd2207abfc9c.

--- example/app/src/main/java/com/assistne/example/MainActivity.java
+++ example/app/src/main/java/com/assistne/example/MainActivity.java
@@ -1,9 +1,7 @@
 package com.assistne.example;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,21 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void toRow(View view) {
-        startActivity(new Intent(this, RowActivity.class));
-    }
-
-    public void toRowReverse(View view) {
-        startActivity(new Intent(this, RowReverseActivity.class));
-    }
-
-    public void toColumn(View view) {
-        startActivity(new Intent(this, ColumnActivity.class));
-    }
-
-    public void toColumnReverse(View view) {
-        startActivity(new Intent(this, ColumnReverseActivity.class));
     }
 }
