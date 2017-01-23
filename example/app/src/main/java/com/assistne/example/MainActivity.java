72b8e5c55bb3cb8022f8014557d4fd2207abfc9c
AssIstne <liangjiaquan@wbiao.cn>
2017-01-23 16:02:52 +0800

    add some example into demo

--- example/app/src/main/java/com/assistne/example/MainActivity.java
+++ example/app/src/main/java/com/assistne/example/MainActivity.java
@@ -1,7 +1,9 @@
 package com.assistne.example;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toRow(View view) {
+        startActivity(new Intent(this, RowActivity.class));
+    }
+
+    public void toRowReverse(View view) {
+        startActivity(new Intent(this, RowReverseActivity.class));
+    }
+
+    public void toColumn(View view) {
+        startActivity(new Intent(this, ColumnActivity.class));
+    }
+
+    public void toColumnReverse(View view) {
+        startActivity(new Intent(this, ColumnReverseActivity.class));
     }
 }
---
+++ example/app/src/main/java/com/assistne/example/RowReverseActivity.java
@@ -0,0 +1,13 @@
+package com.assistne.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RowReverseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_row_reverse);
+    }
+}
