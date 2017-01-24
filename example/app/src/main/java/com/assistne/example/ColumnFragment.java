b3842c76c4c29b69d65d601fde7fa2d3a82198ac
AssIstne <liangjiaquan@wbiao.cn>
2017-01-24 16:44:17 +0800

    change view state in demo

--- example/app/src/main/java/com/assistne/example/ColumnFragment.java
+++ example/app/src/main/java/com/assistne/example/ColumnFragment.java
@@ -2,10 +2,12 @@ package com.assistne.example;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -26,4 +28,22 @@ public class ColumnFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_column, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final Button button = (Button) view.findViewById(R.id.button);
+        final ViewGroup main = (ViewGroup) view.findViewById(R.id.main);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!main.isSelected()) {
+                    button.setText("Unselected");
+                    main.setSelected(true);
+                } else {
+                    button.setText("Selected");
+                    main.setSelected(false);
+                }
+            }
+        });
+    }
 }
